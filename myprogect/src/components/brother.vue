ac0795417ad4accf2aa7117ba3fb1d7eb5861196
Artanic <599253487@qq.com>
2018-07-19 11:44:54 +0800

    updata

--- myprogect/src/components/brother.vue
+++ myprogect/src/components/brother.vue
@@ -3,19 +3,30 @@
       <h2>我是一个brother</h2>
       <br>
       <button @click="tobrother">给sister传递数据</button>
+      <br>
+      <input v-model="receipt">
     </div>
 </template>
 
 <script>
+  import talk from './model/communication'
     export default {
         data () {
           return {
+            msg: 'i am brother',
+            receipt: ''
           }
         },
       methods: {
           tobrother() {
-
+             talk.$emit('tosister', this.msg)
           }
+      },
+      mounted () {
+       talk.$on('tobrother', function (data) {
+        console.log(data)
+        this.receipt = data
+      }.bind(this))
       }
     }
 </script>
--- myprogect/src/components/sister.vue
+++ myprogect/src/components/sister.vue
@@ -1,13 +1,35 @@
 <template>
     <div class="sister">
       <h2>我是一个sister</h2>
+      <br>
+       <button @click="tosister">给brother传递数据</button>
+      <br>
+      <input v-model="receipt">
     </div>
 </template>
 
 <script>
+  import talk from './model/communication'
     export default {
-        name: "brother"
-    }
+      data() {
+        return {
+          receipt: '',
+          msg: 'i sm sister'
+        }
+      },
+      mounted () {
+        talk.$on('tosister', function (data) {
+        console.log(data)
+        this.receipt = data
+      }.bind(this))    //加入.bind(this)
+       // https://www.cnblogs.com/sunsanfeng/p/emit.html//
+      },
+      methods: {
+        tosister () {
+           talk.$emit('tobrother', this.msg)
+        }
+      }
+   }
 </script>
 
 <style scoped>
